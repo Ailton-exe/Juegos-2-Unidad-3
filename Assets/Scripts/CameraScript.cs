bfd8b522c702025052b37a9ff51c8ebe9be0af72
Ailton-exe <ailton.ramos23b@udgvirtual.udg.mx>
2025-10-18 16:12:11 -0600

    3.7. Efectos de acción

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+
+{
+    public GameObject John;
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = John.transform.position.x;
+        transform.position = position;
+    }
+    
+}
